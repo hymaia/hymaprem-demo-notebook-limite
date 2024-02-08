f9cfa0ed3b193bff91512db40a938e5097cda5e8
Franck Cussac <fcussac.ext@galerieslafayette.com>
2024-02-08 11:11:13 +0100

    resolve demo

--- src/main.py
+++ src/main.py
@@ -1,9 +1,43 @@
+import pandas as pd
+from sklearn.tree import DecisionTreeClassifier
+
+
+FEATURES = ["Age", "Fare", "Sex", "Pclass"]
+
+
+def encode_sex(df: pd.DataFrame) -> pd.DataFrame:
+    res = df.copy()
+    res["Sex"] = df['Sex'].map({'male': 0, 'female': 1})
+    return res
+
+
 def main():
-    print(hello_world())
+    train_df = pd.read_csv("src/resources/train.csv")
+    test_df = pd.read_csv("src/resources/test.csv")
+
+    predictions_df = predictions(test_df, train_df)
+    predictions_df.to_csv("titanic_preds.csv", index=False)
 
 
-def hello_world():
-    return "Hello world!"
+def predictions(test_df: pd.DataFrame, train_df: pd.DataFrame) -> pd.DataFrame:
+    encoded_test_df = encode_sex(test_df)
+    encoded_train_df = encode_sex(train_df)
+
+    model = DecisionTreeClassifier()
+
+    X = encoded_train_df[FEATURES]
+    y = encoded_train_df["Survived"]
+
+    model.fit(X, y)
+    predictions_df = get_predictions(encoded_test_df, model)
+    return predictions_df
+
+
+def get_predictions(df: pd.DataFrame, model: DecisionTreeClassifier) -> pd.DataFrame:
+    pred_col = model.predict(df[FEATURES])
+    res = df.copy()[["PassengerId"]]
+    res["Survived"] = pred_col
+    return res
 
 
 if __name__ == "__main__":
--- tests/test_main.py
+++ tests/test_main.py
@@ -1,12 +1,51 @@
-from src.main import hello_world
+from unittest.mock import MagicMock
+
+import numpy as np
+import pandas as pd
+from sklearn.tree import DecisionTreeClassifier
+
+from src.main import encode_sex, get_predictions
 
 
-def test_hello_world():
+def test_encode_sex():
     # Given
-    expected = "Hello world!"
+    input = pd.DataFrame({
+        "Sex": ["male", "female"],
+    })
+    expected = pd.DataFrame({
+        "Sex": [0, 1],
+    })
+    expected_not_mutable_input = input.copy()
 
     # When
-    actual = hello_world()
+    actual = encode_sex(input)
 
     # Then
-    assert actual == expected
+    pd.testing.assert_frame_equal(actual, expected)
+    pd.testing.assert_frame_equal(input, expected_not_mutable_input)
+
+
+def test_get_predictions():
+    # Given
+    input_df = pd.DataFrame({
+        "PassengerId": [1, 2, 3],
+        "Age": [22, 27, 32],
+        "Fare": [7.25, 13.85, 69.55],
+        "Sex": [0, 1, 0],
+        "Pclass": [3, 2, 1],
+    })
+    input_model = DecisionTreeClassifier()
+    input_model.predict = MagicMock(return_value=1)
+
+    expected = pd.DataFrame({
+        "PassengerId": [1, 2, 3],
+        "Survived": [1, 1, 1],
+    })
+    expected_not_mutable_input = input_df.copy()
+
+    # When
+    actual = get_predictions(input_df, input_model)
+
+    # Then
+    pd.testing.assert_frame_equal(actual, expected)
+    pd.testing.assert_frame_equal(input_df, expected_not_mutable_input)
